d5a6cf6842feebcad2097f85ee274e99e9951ce7
Daniar <tbylat@gmail.com>
2022-10-11 16:44:38 +0500

    homework

---
+++ task 19.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Console.WriteLine("Введите пятизначное число: ");
+// string num = Convert.ToString(Console.ReadLine());
+// int size = num.Length;
+// if (size == 5)
+// {
+//     if (num[0] == num[4] && num[1] == num[3])
+//     {
+//             Console.WriteLine("Является палиндромом");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Не является палиндромом");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Введено неверное число");
+// }
+
---
+++ task 21.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// Console.Write("Введите значение координаты x первой точки: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение координаты y первой точки: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение координаты z первой точки: ");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение координаты x второй точки: ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение координаты y второй точки: ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение координаты z второй точки: ");
+// int z2 = Convert.ToInt32(Console.ReadLine());
+
+// int sum = ((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1))+((z2-z1)*(z2-z1));
+// double distance = Math.Round(Math.Sqrt(sum),2);
+
+// System.Console.WriteLine($"Расстояние между точками = {distance}");
